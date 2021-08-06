6076f42dc570895849a9420ef78f14c2c159835c
ingbunga <20sunrin129@sunrint.hs.kr>
2021-08-07 04:18:07 +0900

    stableSort

--- Template.js
+++ Template.js
@@ -1,14 +1,19 @@
 const fs = require('fs')
 const stdin = fs.readFileSync('dev/stdin').toString().split('\n');
  
+// INPUT FOR BOJ. works like input in python.
 const input = (() => {
     let line = 0;
     return () => stdin[line++];
 })();
 
+// works like reversed in python.
 const reversed = l => [...l].reverse();
 
 
+function stableSort(arr,key=e=>e){
+    return arr.map((e,i)=>[e,i]).sort((a,b)=>key(a[0])===key(b[0])?a[1]-b[1]:key(a[0])-key(b[0])).map(e=>e[0]);
+}
 
 
 
